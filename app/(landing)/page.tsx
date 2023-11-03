4801b5ea918927a711aafcdce1515d2095caa1db
Mudit Kapoor <muditk441@gmail.com>
2023-11-03 07:40:18 +0530

    landing page

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,20 +1,15 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import { LandingContent } from "@/components/landing-content";
+import { LandingNavbar } from "@/components/landing-navbar";
+import {LandingHero} from '@/components/landing-hero'
 
 const LandingPage = () => {
-  return (
-    <div>
-      <div>Landing Page</div>
-      <div>
-        <Link href="/sign-in">
-          <Button>Login</Button>
-        </Link>
-        <Link href="/sign-up">
-          <Button>Register</Button>
-        </Link>
-      </div>
+  return ( 
+    <div className="h-full ">
+      <LandingNavbar />
+      <LandingHero />
+      <LandingContent />
     </div>
-  );
-};
-
+   );
+}
+ 
 export default LandingPage;
