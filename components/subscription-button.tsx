a417f3765e375b1c92bc302586c3eba13ec5e906
Mudit Kapoor <muditk441@gmail.com>
2023-11-04 08:12:13 +0530

    stripe part also done

--- components/subscription-button.tsx
+++ components/subscription-button.tsx
@@ -4,17 +4,22 @@ import axios from "axios";
 import { useState } from "react";
 import { Zap } from "lucide-react";
 import { toast } from "react-hot-toast";
-
+import { useUser } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
 
 export const SubscriptionButton = ({ isPro = false }: { isPro: boolean }) => {
   const [loading, setLoading] = useState(false);
+  const { user } = useUser();
+  const userId = user?.id; // Now you have the userId on the client sid
 
   const onClick = async () => {
     try {
       setLoading(true);
 
-      const response = await axios.get("/api/stripe");
+      const response = await axios.post("http://localhost:8000/api/stripe" , {
+        userId : userId ,
+        user : user
+      });
 
       window.location.href = response.data.url;
     } catch (error) {
